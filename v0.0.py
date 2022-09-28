67bf59cc7a930071e25cf850167e1e371d705069
Dmitry Nechman <Nechman.D@yandex.ru>
2022-09-28 17:03:30 +0500

    Добавлен базовый функционал бота

--- v0.0.py
+++ v0.0.py
@@ -1,1 +1,17 @@
-'Чистовой проект, здесь будет бот ТГ'
+
+from aiogram import Bot, Dispatcher, executor, types
+
+# Объект бота
+bot = Bot(token="5648590997:AAHYn7hqS7ZJMfpQmwC7wgq6BGAFuN8qY4E")
+# Диспетчер для бота
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands="start")
+async def cmd_start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Обучение", "Проверка"]
+    keyboard.add(*buttons)
+    await message.answer("Какой режим вы бы хотели попробовать?", reply_markup=keyboard)
+
+if __name__ == '__main__':
+   executor.start_polling(dp, skip_updates=True)
