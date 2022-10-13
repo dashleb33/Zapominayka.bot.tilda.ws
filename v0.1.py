f66aef6b82b9d5c0d0b8d6bff9071c1b41fbfc6e
Dmitry Nechman <Nechman.D@yandex.ru>
2022-10-13 14:00:26 +0500

    feat (XQUW-57): добавлена "подсказка" в режим игры, укрупнены блоки и написаны комментарии к коду

--- v0.1.py
+++ v0.1.py
@@ -16,6 +16,9 @@ europe = sqlite3.connect('europe_capitals.db', check_same_thread=False)
 cursor = conn.cursor()
 cursor_db = europe.cursor()
 lst = []
+
+
+#  Машина состояний
 class Form(StatesGroup):
     teaching = State()
     ask_eche_primer = State()
@@ -27,15 +30,17 @@ class Form(StatesGroup):
 
 # добавление пользователя в базу данных
 def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
-    cursor.execute('INSERT INTO test (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
+    cursor.execute('INSERT INTO test (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)',
+                   (user_id, user_name, user_surname, username))
     conn.commit()
 
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Привет!\nЯ твой бот!\n Используйте /help, чтобы узнать список всех команд")
+    await message.reply("Привет!\nЯ ваш бот!\n Используйте /help, чтобы узнать список всех команд")
 
 
+# Меню HELP
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Доступные следующие команды:\n /register для регистрации  \n"
@@ -46,28 +51,20 @@ async def process_help_command(message: types.Message):
                         "/create - создать свои мнемонические карточки")
 
 
-
+# Раздел register
 @dp.message_handler(commands=['register'])
 async def process_registration(message: types.Message):
-        await message.reply('Приступим к регистрации!\nЭто нужно, чтобы ваши мнемонические правила и прогресс сохранялись')
-        us_id = message.from_user.id
-        us_name = message.from_user.first_name
-        us_sname = message.from_user.last_name
-        username = message.from_user.username
-        db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
-        await message.reply('Вы добавлены в базу пользователей!')
+    await message.reply('Приступим к регистрации!\n'
+                        'Это нужно, чтобы ваши мнемонические правила и прогресс сохранялись')
+    us_id = message.from_user.id
+    us_name = message.from_user.first_name
+    us_sname = message.from_user.last_name
+    username = message.from_user.username
+    db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
+    await message.reply('Вы добавлены в базу пользователей!')
 
 
-@dp.message_handler(commands=['train'])
-async def tutorial_guide(message: types.Message):
-        await message.reply("Давайте приступим к обучению. \n Сейчас мы покажем вам примеры мнемонических правил"
-                            " для столиц стран, которые помогут вам обучиться, поставьте +, если готовы")
-        global lst
-        lst = list(range(1, 11))
-        r.shuffle(lst)
-        await Form.teaching.set()
-
-
+# Отмена действия пользователя
 @dp.message_handler(state='*', commands='cancel')
 @dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
 async def cancel_handler(message: types.Message, state: FSMContext):
@@ -78,6 +75,19 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     await message.reply('Cancelled.')
 
 
+# Раздел train
+@dp.message_handler(commands=['train'])
+async def tutorial_guide(message: types.Message):
+        await message.reply("Давайте приступим к обучению. \n "
+                            "Сейчас мы покажем вам примеры мнемонических правил"
+                            " для столиц стран, которые помогут вам обучиться, поставьте +, если готовы")
+        global lst
+        lst = list(range(1, 11))
+        r.shuffle(lst)
+        await Form.teaching.set()
+
+
+# Продолжение раздела train
 @dp.message_handler(state=Form.teaching)
 async def show_examples(message: types.Message, state: FSMContext):
     if lst:
@@ -92,6 +102,7 @@ async def show_examples(message: types.Message, state: FSMContext):
             await message.reply(f'Этот пример был последним, перейдите, пожалуйста, в другой раздел /help')
 
 
+# Продолжение раздела train, ещё пример показать?
 @dp.message_handler(state=Form.ask_eche_primer)
 async def asking(message: types.Message, state: FSMContext):
     answer = message.text
@@ -101,6 +112,8 @@ async def asking(message: types.Message, state: FSMContext):
         await message.reply('Выберете другое в меню.')
         await state.finish()
 
+
+# Раздел "новая игра", генерация последовательности
 @dp.message_handler(commands=['newplay'])
 async def tutorial_guide(message: types.Message):
     global play_primers
@@ -108,6 +121,8 @@ async def tutorial_guide(message: types.Message):
     r.shuffle(play_primers)
     await message.reply('Мы сгененировали уникальную последовательность для вас, нажмите /play для игры')
 
+
+# раздел "новая игра", начало игры
 @dp.message_handler(commands=['play'])
 async def tutorial_guide(message: types.Message):
     global capital_for_play
@@ -122,34 +137,22 @@ async def tutorial_guide(message: types.Message):
     await Form.play.set()
 
 
+# раздел "игра", проверка ответа
 @dp.message_handler(state=Form.play)
 async def asking(message: types.Message, state: FSMContext):
     answer = message.text
     if answer.lower() in capital_for_play.lower():
         await message.reply(f'Верно! для продолжения /play, для выхода /cancel ')
         await state.finish()
+    elif answer == '/hint':
+        await message.reply(f'{capital_for_play}, для продолжения игры /play')
+        await state.finish()
     else:
-        await message.reply('Неправильно, попробуйте ещё раз, для выхода /cancel, для получения ответа /otvet')
-
-@dp.message_handler(commands=['otvet'])
-async def tutorial_guide(message: types.Message):
-    await message.reply(f'{capital_for_play}, для продолжения игры /play')
+        await message.reply('Неправильно, попробуйте ещё раз, для выхода /cancel, для получения ответа /hint')
 
 
 
-
-async def show_examples(message: types.Message, state: FSMContext):
-    if lst:
-        a = lst.pop()
-        cursor_db.execute(f'SELECT * FROM capitals WHERE  ID = {a}')
-        data = cursor_db.fetchone()
-        await message.reply(f'Страна: {data[1]} \n Cтолица: {data[2]} \n Мнемоническое правило: {data[3]}')
-        if lst:
-            await message.reply(f'Показать ещё пример? \n Для выхода нажмите /cancel')
-            await Form.ask_eche_primer.set()
-        else:
-            await message.reply(f'Этот пример был последним, перейдите, пожалуйста, в другой раздел /help')
-
+# раздел "правила", показать мнемонические правила
 @dp.message_handler(commands=['configure'])
 async def process_registration(message: types.Message):
     await message.reply('Покажем все мнемонические правила, которые у вас есть')
@@ -157,6 +160,8 @@ async def process_registration(message: types.Message):
     data = cursor_db.fetchall()
     await message.reply(f'{data}')
 
+
+# раздел "пустые правила", показать страны для которых нет правил
 @dp.message_handler(commands=['show_empty'])
 async def process_registration(message: types.Message):
     await message.reply('Сейчас мы покажем все страны и их столицы, для которых у вас нет мнемонических правил')
@@ -165,19 +170,23 @@ async def process_registration(message: types.Message):
     await message.reply(data)
     await message.reply('Если хотите создать правило, выберите /create')
 
+
+# раздел "правила", создать новое правило
 @dp.message_handler(commands=['create'])
 async def process_registration(message: types.Message):
     await message.reply('Сейчас вы создадите свое мнемоническое правило')
     await Form.strana.set()
     await message.reply('Введите страну, регистр неважен')
 
+
+# раздел "правила", проверка, что правила нет
 @dp.message_handler(state=Form.strana)
 async def chose_strana(message: types.Message, state: FSMContext):
     answer = message.text
     cursor_db.execute(f"SELECT Country, Capital  FROM capitals WHERE mnemonic_rule IS NULL")
     data = cursor_db.fetchall()
     for i in data:
-        i =[j.lower() for j in i]
+        i = [j.lower() for j in i]
         if answer.lower() in i:
             global strana1
             strana1 = answer
@@ -188,6 +197,8 @@ async def chose_strana(message: types.Message, state: FSMContext):
         await message.reply('Такой страны нет или мнемоническое правило уже установлено введите другую. \n'
                             'Для выхода из режима выберите /cancel ')
 
+
+# раздел "правила", создание правила
 @dp.message_handler(state=Form.pravilo)
 async def ust_pravilo(message: types.Message, state: FSMContext):
     prpr = message.text
@@ -198,10 +209,11 @@ async def ust_pravilo(message: types.Message, state: FSMContext):
     await message.reply(f'Правило для {strana1} успешно создано "{prpr}"')
 
 
+# хэндлер для остальных сообщений
 @dp.message_handler()
 async def process_registration(message: types.Message):
-        await message.reply('Пожалуйста, выберете команду из меню, для вызова команд наберите /help')
+    await message.reply('Пожалуйста, выберете команду из меню, для вызова команд наберите /help')
 
 
 if __name__ == '__main__':
-   executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True)
